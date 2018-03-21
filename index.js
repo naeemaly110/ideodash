b776eb94b9c42126e1f37b1576f0eef3ebbb22a4
naeemaly110 <naeemali.110@gmail.com>
2018-03-21 12:43:49 +0500

    21-3

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import { graphqlExpress , graphiqlExpress } from "apollo-server-express";
 import { makeExecutableSchema }  from "graphql-tools";
 import models from "./models";
 import path from 'path';
+import cors from 'cors';
 import { fileLoader, mergeTypes ,mergeResolvers  } from 'merge-graphql-schemas';
 
 const typeDefs = mergeTypes(fileLoader(path.join(__dirname, './schema')));
@@ -17,6 +18,8 @@ const schema = makeExecutableSchema({
     typeDefs,
     resolvers
 });
+
+app.use(cors('*'));
 
 app.use('/graphql', bodyParser.json(), graphqlExpress({
     schema: schema,
